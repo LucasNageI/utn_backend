828a819ac0e5232eb7a8e0b1d9e0dcaabe3f18ca
LucasNagel <lucasanagel@gmail.com>
2024-09-25 22:04:18 -0300

    clase 13

--- clase-11-node/index.js
+++ clase-11-node/index.js
@@ -2,14 +2,36 @@
 
 // nodemon es una libreria de desarrollo que permite ejecutar el codigo cada vez que guardemos
 
+import express from "express";
+import filesystem from "fs"
 
-const filesystem = require("fs")
-const { createTxt } = require("./utils/fileSystemManager")
+//se crea una instancia de servidor HTTP
+const app = express()
+const PORT = 4000
 
-console.log("hola mundo")
+//la callback recibe 2 parametros, (request, response)
 
-const objeto = JSON.parse(filesystem.readFileSync("prueba.json", {encoding: 'utf-8'}))
+app.get('/obtener-productos', async (request, response)=>{
+    try{
+        const data= await filesystem.promises.readFile('./public/productos.json', {encoding:'utf-8'})
+        const productos= JSON.parse(data)
+        response.status(200).json({mensaje: 'Lista de productos', code: 200, data: productos})
 
-console.log(objeto.nombre)
+    }catch(error){
+        console.error(error)
+        response.json({
+            mensaje: 'SERVER ERROR: Productos no obtenidos',
+            status: 500,
+            ok: false,
+            data: null
+        })
+    }
+})
 
-createTxt("prueba2", "esta es la prueba 2")
+
+//listen espera recibir 2 valores, el puerto y la callback
+
+app.listen(PORT, () => {
+    //esta callback se ejecuta cuando se este escuchando mi app en el puerto
+    console.log("mi servidor se esta escuchando en el puerto " + PORT)
+})
--- clase-11-node/utils/fileSystemManager.js
+++ clase-11-node/utils/fileSystemManager.js
@@ -1,9 +1,49 @@
+import filesystem from "fs"
 
-const filesystem = require("fs")
+const crearJson = async (fileName, data) => {
+  try {
+       if (typeof fileName !== 'string' || fileName.length < 1) {
+      throw { code: 'ERR_INVALID_ARG_TYPE', message: 'fileName debe ser un string con al menos 1 carácter' }
+    }
+    if (typeof data !== 'object' || data === null) {
+      throw { code: 'ERR_INVALID_ARG_TYPE', message: 'data debe ser un objeto y no puede ser null' }
+    }
 
-const createTxt = (file_name, text) => {
-    const file = file_name + '.txt'
-    filesystem.writeFileSync(file, text, {encoding: 'utf-8'})
+    await filesystem.promises.writeFile(
+      `${fileName}.json`,
+      JSON.stringify(data),
+      { encoding: "utf-8" }
+    )
+    console.log(`Archivo ${fileName}.json creado con éxito.`)
+  } catch (error) {
+    console.dir(error.code)
+    console.error(error.message || "Ocurrio un error excepcional")
+  }
 }
 
-module.exports = {createTxt}
+const leerJson = async (name) => {
+  const nombreJSON = name + ".json"
+  try {
+    const contenido = await filesystem.promises.readFile(nombreJSON, { encoding: "utf-8"})
+
+    const dataJSON = JSON.parse(contenido)
+    console.log(dataJSON)
+  } catch (error) {
+    console.dir(error.code)
+    console.error(error.code === "ENOENT" ? "No se puede leer el archivo" : "Ocurrio un error excepcional")
+  }
+}
+
+export { crearJson, leerJson }
+
+/*app.get("/obtener-usuarios", async (request, response) => {
+    //request es un objeto con todos los datos de la consulta
+    //response es un objeto que se usa para emitir respuestas
+    console.log("recibido")
+
+    const resultado = await filesystem.promises.readFile("./public/usuarios.json", {encoding: "utf-8"})
+    const usuarios = JSON.parse(resultado)
+    console.log(usuarios)
+    response.status(226).json({data: usuarios})
+
+})*/
